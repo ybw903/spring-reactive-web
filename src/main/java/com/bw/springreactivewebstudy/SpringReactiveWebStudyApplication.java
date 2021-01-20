d65392ea36208f039f47c7b06b6513f22a8b400c
병우 유 <conner903@naver.com>
2021-01-20 16:40:56 +0900

    first commit

---
+++ src/main/java/com/bw/springreactivewebstudy/SpringReactiveWebStudyApplication.java
@@ -0,0 +1,13 @@
+package com.bw.springreactivewebstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveWebStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveWebStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bw/springreactivewebstudy/SpringReactiveWebStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bw.springreactivewebstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveWebStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
